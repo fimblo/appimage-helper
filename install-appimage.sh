5bff3bca94d4a4ced15dbc1e49c36906cd44a514
Mattias Jansson <fimblo@yanson.org>
2023-12-12 23:18:01 +0100

    fix: link to correct script

--- install-appimage.sh
+++ install-appimage.sh
@@ -19,6 +19,7 @@ if [[ ! $(basename $1) =~ ^([a-zA-Z]+).*.appimage$ ]]; then
 fi
 
 APP_NAME=${BASH_REMATCH[1]}
+echo $APP_NAME
 
 # Create directories if they don't exist
 mkdir -p "$DEST_DIR" "$BIN_DIR"
@@ -27,9 +28,8 @@ mkdir -p "$DEST_DIR" "$BIN_DIR"
 mv "$FILE_PATH" "$DEST_DIR/"
 echo "Moved $FILE_PATH to $DEST_DIR"
 
-# Extract filename without extension for symlink
-FILENAME=$(basename "$FILE_PATH" | sed -e 's/\..*//')
-# Create symlink
-ln -sf "$DEST_DIR/$FILENAME.appimage" "$BIN_DIR/$APP_NAME"
+
+# Create symlink to helper
+ln -sf "$BIN_DIR/appimage-helper.sh" "$BIN_DIR/$APP_NAME"
 echo "Created symlink in $BIN_DIR for $APP_NAME"
 
