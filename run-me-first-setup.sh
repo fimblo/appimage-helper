f7e53c2a7130c6a4555754a11de82413bf313e5d
Mattias Jansson <fimblo@yanson.org>
2023-12-13 13:56:55 +0100

    fix: install both scripts

--- run-me-first-setup.sh
+++ run-me-first-setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Run this script first to install the appimage helper.
+# Run this script first to setup the appimage helper.
 
 # Variables
 repo_root=$(pwd)
 appimage_dir="$HOME/.local/share/appimages"
-handler_script="appimage-helper.sh"
+runner_script="appimage-helper.sh"
+installer_script="install-appimage.sh"
 
 # Check if in git repository root
 if [ ! -d .git ]; then
-    echo "Error: Please run this installer in the root directory of the git repository."
+    echo "Error: Please run this script in the root of the git repository."
     exit 1
 fi
 
@@ -17,12 +18,14 @@ fi
 echo "Creating AppImage directory at ${appimage_dir}..."
 mkdir -p "$appimage_dir"
 
-# Copy the handler script to a location in PATH
-echo "Installing the handler script..."
+# Copy the two scripts to a location in PATH
+echo "Installing $runner_script and $installer_script..."
 install_path="$HOME/.local/bin"
 mkdir -p "$install_path"
-cp "$repo_root/$handler_script" "$install_path"
-chmod u+x "$install_path/$handler_script"
+cp "$repo_root/$runner_script" "$install_path"
+cp "$repo_root/$installer_script" "$install_path"
+chmod u+x "$install_path/$runner_script"
+chmod u+x "$install_path/$installer_script"
 
 # Add directory to PATH if not already there
 if ! echo "$PATH" | grep -q "$install_path"; then
